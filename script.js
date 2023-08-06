caa13ea31e0c1990053036b29dac52b462f8529e
LivioReb <liviormreboucas@gmail.com>
2023-08-06 10:54:36 -0300

    Conversor de moeda finalizado, com integração a requisição de api

--- script.js
+++ script.js
@@ -1,70 +1,54 @@
-const btn = document.querySelector(".btn");
-const currency_select = document.querySelector(".currency_select")
-console.log(currency_select.value ) 
-const currency_select_main = document.querySelector(".currency_select_main")
+let api = `https://v6.exchangerate-api.com/v6/6ee418975b81ae64c49e2067/latest/USD`;
 
-function convertValues() {
-  
-  const inputCurrencyValue = document.querySelector(".input_currency").value;
-  const currencyValueToConvert = document.querySelector(
-    ".currency_value_toconvert"
-  );
- 
-  const dolarToday = 5.2;
-  const euroToday = 6.2;
-  const realToday = 1;
+const fromDropDown = document.querySelector("#currency_select_main")
+const toDropDown = document.querySelector("#currency_select")
 
-  const currencyValue = document.querySelector(".currency_value");
-  const convertedValue = inputCurrencyValue / dolarToday;
-  const convertedValueEuro = inputCurrencyValue / euroToday;
-  const convertedValueReal = inputCurrencyValue / realToday;
-  
-  if(currency_select.value == "dolar"){
-   
-      currencyValue.innerHTML = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD"
-      }).format(convertedValue)
+
+currencies.forEach((currency) => {
+  const option = document.createElement("option");
+  option.value = currency;
+  option.text = currency;
+  fromDropDown.add(option);
+});
+currencies.forEach((currency) => {
+  const option = document.createElement("option");
+  option.value = currency;
+  option.text = currency;
+  toDropDown.add(option);
+});
+
+fromDropDown.value = "BRL"
+toDropDown.value = "USD"
+
+let convertCurrency = () =>{
+  const valor = document.querySelector(".input_currency");
+  const moeda_p_converter = fromDropDown.value;
+  const moeda_convertida = toDropDown.value;
+  const result = document.querySelector(".currency_value")
+  //Se o campo de entrada não tiver valor = vazio
+
+  if(valor != 0){
+    fetch(api)
+    .then ((resp) => resp.json())
+    .then((data) =>{
+      let taxa_moeda_principal = data.conversion_rates[moeda_p_converter];
+      let taxa_moeda_secundaria = data.conversion_rates[moeda_convertida]
+
+      const convertendoValor = (valor/taxa_moeda_principal) * moeda_convertida
+      result.innerHTML = `${valor} ${moeda_p_converter} = ${convertendoValor.toFixed(2)} ${moeda_convertida}`
+    })
+    .catch((err) => console.log(err))
   }
-  if(currency_select.value == "euro"){  
-      currencyValue.innerHTML = new Intl.NumberFormat("de-DE", {
-        style: "currency",
-        currency: "EUR"
-      }).format(convertedValueEuro)
+  else {
+    alert.("Por favor, preencha algum valor")
   }
-  if(currencyValue.value == "real"){
-    currencyValue.innerHTML = new Intl.NumberFormat("pt-br", {
-        style: "currency",
-        currency: "BR"
-    }).format(convertedValueReal)
-  }
+};
 
-  currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-br", {
-    style: "currency",
-    currency: "BRL"
-  }).format(inputCurrencyValue)
-}
-
-function changeCurrency(){
-const currency_name = document.querySelector("#currency_name")
-const currency_img = document.querySelector("#currency_img")
-
-if(currency_select.value == "dolar"){
-    currency_name.innerHTML = "Dólar americano"
-    currency_img.src = "/projeto1CursoJs/assets/currentys/estados-unidos (1) 1.png"
-}
-if(currency_select.value == "euro"){
-    currency_name.innerHTML = "Euro"
-    currency_img.src = "/projeto1CursoJs/assets/currentys/Design sem nome 3.png"
-}
+document.querySelector(".btn").addEventListener("click", convertCurrency)
+window.addEventListener("load", convertCurrency)
 
 
 
-convertValues()
-
-}
 
 
-currency_select.addEventListener('change', changeCurrency)
 
-btn.addEventListener("click", convertValues);
